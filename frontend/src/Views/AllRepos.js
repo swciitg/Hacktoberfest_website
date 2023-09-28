473741dd46f6e3802323bb0ecc584630234dd48f
Kunalpal216 <kunalpal2115@gmail.com>
2023-09-28 12:38:08 +0530

    form link

--- frontend/src/Views/AllRepos.js
+++ frontend/src/Views/AllRepos.js
@@ -7,84 +7,83 @@ import profile from './profile.svg';
 import swclogo from '../components/LandingPage/hacktober_logo.svg';
 import logout from "./logout.svg"
 const AllRepos = () => {
-    const [iitgRepos, setIitgRepos] = useState();
-    const [nonIitgRepos, setNonIitgRepos] = useState();
-    const [cookies, setCookie, removeCookie] = useCookies(['access_token']);
+  const [iitgRepos, setIitgRepos] = useState();
+  const [nonIitgRepos, setNonIitgRepos] = useState();
+  const [cookies, setCookie, removeCookie] = useCookies(['access_token']);
 
-    useEffect(() => {
-        axios
-          .get(`${BACKEND_API}/api/repo`, {
-            withCredentials: true,
-          })
-          .then((response) => {
-            console.log(response.data);
-            let iitgData = response.data.filter((row,index) => {
-                return row.type==="IITG";
-            })
-            setIitgRepos(iitgData.map((row, index) => {
-                return {
-                  ...row,
-                  index: index + 1,
-                };
-              }));
-            let nonIitgData = response.data.filter((row,index) => {
-                return row.type!=="IITG";
-            })
-            setNonIitgRepos(nonIitgData.map((row, index) => {
-                return {
-                  ...row,
-                  index: index + 1,
-                };
-              }));
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    
-      }, []);
-      
-      const handleLogout = () => {
-        removeCookie('access_token');
-        window.location.href = '/hacktoberfest';
-        // console.log(cookies.access_token)
-      }
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_API}/api/repo`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response.data);
+        let iitgData = response.data.filter((row, index) => {
+          return row.type === "IITG";
+        })
+        setIitgRepos(iitgData.map((row, index) => {
+          return {
+            ...row,
+            index: index + 1,
+          };
+        }));
+        let nonIitgData = response.data.filter((row, index) => {
+          return row.type !== "IITG";
+        })
+        setNonIitgRepos(nonIitgData.map((row, index) => {
+          return {
+            ...row,
+            index: index + 1,
+          };
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    return (
-        <div className="bg-[#2a303c] w-full h-full">
-             <div className="absolute flex items-center top-[8px] right-2 z-10 text-white">
-             <a className="bg-[#ffffff26] text-white font-[20px] p-[15px] rounded-md transition-all hover:scale-105" href ="/hacktoberfest/leaderboard">Leaderboard</a>
+  }, []);
 
-                <a className=" p-[15px] rounded-md transition-all hover:scale-105" href="/hacktoberfest/profile">
-                    <img src={profile} width = {35}></img>
-                </a>
-                <button onClick={handleLogout}>
+  const handleLogout = () => {
+    removeCookie('access_token');
+    window.location.href = '/hacktoberfest';
+    // console.log(cookies.access_token)
+  }
+
+  return (
+    <div className="bg-[#2a303c] w-full h-full">
+      <div className="absolute flex items-center top-[8px] right-2 z-10 text-white">
+        <a className="bg-[#ffffff26] text-white font-[20px] p-[15px] rounded-md transition-all hover:scale-105" href="/hacktoberfest/leaderboard">Leaderboard</a>
+
+        <a className=" p-[15px] rounded-md transition-all hover:scale-105" href="/hacktoberfest/profile">
+          <img src={profile} width={35}></img>
+        </a>
+        <button onClick={handleLogout}>
           <img src={logout} width={40}></img>
         </button>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="h-2/5 w-full bg-[#170f1e] flex flex-col items-center">
+          <div className="items-center p-6">
+            <span className="text-white font-bold text-[38.4px]">All Repos</span>
+            <hr color="white"></hr>
+          </div>
+          <div>
+            <div className="pt-24 pb-48">
+              <img src={swclogo} width={800}></img>
             </div>
-            <div className="flex flex-col items-center">
-                <div className="h-2/5 w-full bg-[#170f1e] flex flex-col items-center">
-                    <div className="items-center p-6">
-                            <span className="text-white font-bold text-[38.4px]">All Repos</span>
-                            <hr color="white"></hr>
-                    </div>
-                    <div>
-                        <div className="pt-24 pb-48">
-                            <img src={swclogo} width={800}></img>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="md:pl-24 pl-24 bg-[#170f1e]">
-                <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-                {iitgRepos?.map(row => (
-                <Card row={row} key={row.index}/>
-                ))}
-                </div>
-            </div>
-            <p className="text-white pt-5 pl-20 bg-[#170f1e] text-[20px]">If you wish to make a contribution to another repository, please complete this form. </p>
-            <p className="text-white pb-5 pl-20 bg-[#170f1e] text-[20px]">If your submission is deemed valid, we will gladly accept it:</p>
+          </div>
         </div>
-    );
+      </div>
+      <p className="text-white pt-5 pl-20 bg-[#170f1e] text-[20px]">If you find any more repo suitable to be included here then, fill out this <a href="https://forms.office.com/r/YzX1rQPs2b">form</a></p>
+      <div className="md:pl-24 pl-24 bg-[#170f1e]">
+        <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+          {iitgRepos?.map(row => (
+            <Card row={row} key={row.index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AllRepos;
