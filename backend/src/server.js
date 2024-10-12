3f395ead4ff2673768ef2aa9a9b8560616e69115
Aditya Dadhich <adidadhich2003@gmail.com>
2024-10-12 22:21:28 +0530

    minor_changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -85,11 +85,11 @@ app.get(process.env.BASE_API_PATH + '/leaderboard', async (req, res) => {
           username,
           avatar_url,
           total_pr_merged,
-          total_points 
+          total_points
         });
       }
     }
-    leaderboardData.sort((a,b) => b.total_pr_merged-a.total_pr_merged);
+    leaderboardData.sort((a, b) => b.total_pr_merged - a.total_pr_merged);
     console.log(leaderboardData);
     res.send(leaderboardData);
   } catch (error) {
@@ -158,19 +158,19 @@ app.get(process.env.HOME_PATH + '/auth/github/callback',
 
 app.use((req, res, next) => {
   try {
-    if(req.originalUrl===process.env.BASE_API_PATH + '/profile'){
-    console.log('Cookies: ', req.cookies);
-    if (req.cookies.access_token!==undefined) {
-      var decoded = jwt.verify(req.cookies.access_token, process.env.SECRET_KEY);
-      console.log(decoded)
-      req.access_token = decoded
-      next();
-    } else {
-      console.log("no token found");
-      res.redirect(process.env.HOME_PATH + '/auth/github');
+    if (req.originalUrl === process.env.BASE_API_PATH + '/profile') {
+      console.log('Cookies: ', req.cookies);
+      if (req.cookies.access_token !== undefined) {
+        var decoded = jwt.verify(req.cookies.access_token, process.env.SECRET_KEY);
+        console.log(decoded)
+        req.access_token = decoded
+        next();
+      } else {
+        console.log("no token found");
+        res.redirect(process.env.HOME_PATH + '/auth/github');
+      }
     }
-  }
-  else next();
+    else next();
   } catch (err) {
     console.log(err);
     res.redirect(process.env.HOME_PATH + '/auth/github');
@@ -200,81 +200,85 @@ async function updateLeaderboard() {
   const tokens = await UserTokenInfo.find({}).exec();
   const repoArray = [];
   const repos = await HacktoberRepo.find({}).exec();
-  try{
-  const tokenArray = tokens.map(token => token.access_token);
-  const randomIndex = Math.floor(Math.random() * tokenArray.length);
-  for (const repo of repos) {
-    const repo_name_owner = await getRepo.getRepo_owner_name(repo.repo_id, tokenArray[randomIndex]);
-    repo.owner = repo_name_owner.data.owner.login;
-    repo.repo = repo_name_owner.data.name;
-    const repoObject = {
-      name: repo_name_owner.data.name,
-      owner: repo_name_owner.data.owner.login,
-    };
-    repoArray.push(repoObject);
-    await repo.save();
+  try {
+    const tokenArray = tokens.map(token => token.access_token);
+    const randomIndex = Math.floor(Math.random() * tokenArray.length);
+    for (const repo of repos) {
+      const repo_name_owner = await getRepo.getRepo_owner_name(repo.repo_id, tokenArray[randomIndex]);
+      repo.owner = repo_name_owner.data.owner.login;
+      repo.repo = repo_name_owner.data.name;
+      const repoObject = {
+        name: repo_name_owner.data.name,
+        owner: repo_name_owner.data.owner.login,
+      };
+      repoArray.push(repoObject);
+      await repo.save();
+    }
+    await getRepo.getPRCountsForMultipleRepos(repos, tokenArray[randomIndex]);
+    for (const access_token of tokenArray) {
+      const userInfo = await UserTokenInfo.findOne({ access_token: access_token });
+      const userData = await getUserInfo(access_token);
+      if (userData === undefined) { // token invalid
+        continue;
+      }
+      //console.log(userData);
+      User.findOne({ github_id: userInfo.github_id })
+        .exec()
+        .then((existingUser) => {
+          if (existingUser) {
+            existingUser.github_username = userData.login;
+            existingUser.avatar_url = userData.avatar_url;
+            return existingUser.save();
+          }
+        });
+      const labels = await githubLabels.find({}).exec();
+      const username = userData.login;
+      let total_pr_merged = 0;
+      let points = 0;
+      for (const repo of repoArray) {
+        const [merged_pr_Data] = await countPullRequestsForUserAndRepo(username, repo, access_token, labels);
+        console.log(merged_pr_Data);
+        repo.repo_mergedPR_counts = merged_pr_Data.count;
+        total_pr_merged += merged_pr_Data.total_count;
+        
+        // for (const pr of merged_pr_Data) {
+        //   for (const label of pr.labels) {
+        //     if (label.name === 'easy') {
+        //       points += 5;
+        //     } else if (label.name === 'medium') {
+        //       points += 10;
+        //     } else if (label.name === 'hard') {
+        //       points += 20;
+        //     }
+        //   }
+        // }
+      }
+
+      UserLeaderboard.findOne({ github_id: userInfo.github_id })
+        .exec()
+        .then((existingLeaderboardData) => {
+          if (existingLeaderboardData) {
+            existingLeaderboardData.pull_requests_merged = total_pr_merged;
+            existingLeaderboardData.points = points;
+            return existingLeaderboardData.save();
+          } else {
+            const leaderboardData = new UserLeaderboard({
+              github_id: userInfo.github_id,
+              pull_requests_merged: total_pr_merged,
+              points: points
+            });
+            return leaderboardData.save();
+          }
+        })
+        .then(() => {
+          console.log("Leaderboard data saved successfully");
+        });
+    }
   }
-  await getRepo.getPRCountsForMultipleRepos(repos, tokenArray[randomIndex]);
-  for (const access_token of tokenArray) {
-    const userInfo = await UserTokenInfo.findOne({ access_token: access_token });
-    const userData = await getUserInfo(access_token);
-    if(userData===undefined){ // token invalid
-      continue;
-    }
-    console.log(userData);
-    User.findOne({ github_id: userInfo.github_id })
-      .exec()
-      .then((existingUser) => {
-        if (existingUser) {
-          existingUser.github_username = userData.login;
-          existingUser.avatar_url = userData.avatar_url;
-          return existingUser.save();
-        }
-      });
-    const labels = await githubLabels.find({}).exec();
-    const username = userData.login;
-    let total_pr_merged = 0;
-    for (const repo of repoArray) {
-      const [merged_pr_Data] = await countPullRequestsForUserAndRepo(username, repo, access_token, labels);
-      repo.repo_mergedPR_counts = merged_pr_Data.count;
-      total_pr_merged += merged_pr_Data.total_count;
-    }
-    // let points = 0;
-    // for (const pr of merged_pr_data) {
-    //   for (const label of pr.labels) {
-    //     if (label.name === 'easy') {
-    //       points += 5;
-    //     } else if (label.name === 'medium') {
-    //       points += 10;
-    //     } else if (label.name === 'hard') {
-    //       points += 20;
-    //     }
-    //   }
-    // }
-    UserLeaderboard.findOne({ github_id: userInfo.github_id })
-      .exec()
-      .then((existingLeaderboardData) => {
-        if (existingLeaderboardData) {
-          existingLeaderboardData.pull_requests_merged = total_pr_merged;
-          existingleaderboarddata.points = points;
-          return existingLeaderboardData.save();
-        } else {
-          const leaderboardData = new UserLeaderboard({
-            github_id: userInfo.github_id,
-            pull_requests_merged: total_pr_merged,
-            points: points
-          });
-          return leaderboardData.save();
-        }
-      })
-      .then(() => {
-        console.log("Leaderboard data saved successfully");
-      });
-    }}
-    catch (err){
-      updateLeaderboard(); // again call -> with diff random index
-      console.error("Error while saving leaderboard data:", err);
-    }
+  catch (err) {
+    //updateLeaderboard(); // again call -> with diff random index
+    console.error("Error while saving leaderboard data:", err);
+  }
 }
 
 app.get(process.env.BASE_API_PATH + '/profile', async (req, res) => {
@@ -314,7 +318,7 @@ app.put(process.env.BASE_API_PATH + "/profile", async (req, res) => {
   let body = req.body;
   let userInfo = await getUserInfo(req.access_token);
   console.log(userInfo);
-  if(userInfo===undefined){ // token invalid
+  if (userInfo === undefined) { // token invalid
     res.redirect(process.env.HOME_PATH + '/auth/github');
     return;
   }
@@ -347,12 +351,12 @@ app.put(process.env.BASE_API_PATH + "/profile", async (req, res) => {
 
 app.post(process.env.BASE_API_PATH + '/label', async (req, res) => {
   if (req.headers["moderator-key"] === process.env.MODERATOR_KEY) {
-    const {label} = req.body;
-    const githubLabelInfo = githubLabels({label_type: label});
+    const { label } = req.body;
+    const githubLabelInfo = githubLabels({ label_type: label });
     await githubLabelInfo.save();
-    res.json({success: true, message : "posted label successfully"});
+    res.json({ success: true, message: "posted label successfully" });
   }
-  else{
+  else {
     return res.status(403).json({
       error: 'Invalid secret key.'
     });
@@ -368,17 +372,17 @@ app.post(process.env.BASE_API_PATH + '/repo', async (req, res) => {
       type
     } = req.body;
     try {
-    if (!owner || !repo || !type) {
-      return res.status(400).json({
-        error: 'Please fill all the three entries!'
-      });
-    }
-    const tokens = await UserTokenInfo.find({}).exec();
-    const tokenArray = tokens.map(token => token.access_token);
-    const randomIndex = Math.floor(Math.random() * tokenArray.length);
-    const repo_info = await getRepo.getRepoInfo(owner, repo, tokenArray[randomIndex]);
-    console.log(repo_info);
-    const repo_id = repo_info.id;
+      if (!owner || !repo || !type) {
+        return res.status(400).json({
+          error: 'Please fill all the three entries!'
+        });
+      }
+      const tokens = await UserTokenInfo.find({}).exec();
+      const tokenArray = tokens.map(token => token.access_token);
+      const randomIndex = Math.floor(Math.random() * tokenArray.length);
+      const repo_info = await getRepo.getRepoInfo(owner, repo, tokenArray[randomIndex]);
+      console.log(repo_info);
+      const repo_id = repo_info.id;
       const existingRepo = await HacktoberRepo.findOne({
         repo_id
       });
@@ -419,7 +423,7 @@ app.delete(process.env.BASE_API_PATH + '/repo', async (req, res) => {
     const { owner, repo } = req.body;
 
     try {
-      
+
       if (!owner || !repo) {
         return res.status(400).json({
           error: 'Please provide both the owner and the repo name!'
@@ -436,7 +440,7 @@ app.delete(process.env.BASE_API_PATH + '/repo', async (req, res) => {
         });
       }
 
-     
+
       await HacktoberRepo.deleteOne({ _id: repoToDelete._id });
 
       return res.status(200).json({
